a6839f80106b283c85323a35856b68f157b54db6
Aman <sharmaaman4419@gmail.com>
2023-05-08 21:43:46 +0530

    cosmetic changes

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const app = express();
+const PORT = 3000;
+const userRoutes = require("./routes/users");
+const blogRoutes = require("./routes/blogs");
+
+const { checkAuthTokenInHeaders } = require("./services/middlewares");
+const { connectionToDb } = require("./db/database.connection");
+
+require("dotenv").config();
+
+// Db connection
+connectionToDb(process.env.DB_URL).then(() => {
+  return console.log("Connected to Db");
+});
+
+// Middle ware for template engine setup
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+// Middle ware to set teh json raw data into req body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Middle ware to serve the static files
+app.use(express.static("./views"));
+app.use(express.static("./public"));
+// Middle ware Parsing the cookies
+app.use(cookieParser());
+
+// Middle ware for token verification and getting the paylod
+app.use(checkAuthTokenInHeaders("token"));
+
+// Rendering the homepage
+app.get("/", (req, res) => {
+  res.render("homepage", { user: req.user });
+});
+
+// Routes
+app.use("/", userRoutes);
+app.use("/", blogRoutes);
+
+// Listening the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
